d3e2be5991b8bc9ca0c1f2e36179601e5524f3cf
Mark Howells-Mead <mark@sayhello.ch>
2020-03-16 13:15:21 +0100

    Update index.js

--- components/imageselector/index.js
+++ components/imageselector/index.js
@@ -19,119 +19,105 @@
 />
  */
 
-import { _x } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
-import { MediaUpload, MediaUploadCheck } from '@wordpress/data';
+import {
+	Button
+} from '@wordpress/components';
+import {
+	Component,
+	Fragment
+} from '@wordpress/element';
+import {
+	MediaUploadCheck,
+	MediaUpload
+} from '@wordpress/block-editor';
+import {
+	_x
+} from '@wordpress/i18n';
 
-import { getLazySrcs } from './LazyImage.jsx';
+import {
+	getLazySrcs
+} from './LazyImage.jsx';
 
 export class ImageSelector extends Component {
-	constructor(props) {
-		super(...arguments);
+
+	constructor( props ) {
+		super( ...arguments );
 		this.props = props;
 	}
 
 	render() {
+
 		const {
 			allowedTypes,
 			attributes,
 			attributeKey,
-			figureClassName,
 			image,
 			imageFormat,
 			setAttributes
 		} = this.props;
 
-		const allowed_types = allowedTypes || ['image/jpg', 'image/jpeg'];
+		const allowed_types = allowedTypes || [ 'image/jpg', 'image/jpeg' ];
 		const attribute_key = attributeKey || 'image';
-		const figure_class_name = figureClassName || '';
 		const image_format = imageFormat || 'full';
 
 		return (
-			<div class="o-imageselector">
-				<MediaUploadCheck>
-					<MediaUpload
-						onSelect={image => {
-							getLazySrcs(image.id, image_format).then(image =>
-								setAttributes({
-									[attribute_key]: image
-								})
-							);
-						}}
-						allowedTypes={allowed_types}
-						value={[attribute_key].id}
-						render={({ open }) => {
-							return (
-								<figure
-									className={`o-imageselector__figure ${
-										!image.id
-											? 'o-imageselector__figure--noimage'
-											: ''
-									} ${figure_class_name}`}
-								>
-									{image.id && (
-										<img
-											className="o-imageselector__image"
-											onClick={open}
-											src={image.org[0]}
-											alt={image.alt}
-										/>
-									)}
-									<div class="o-imageselector__buttons">
-										{!image.id && (
-											<Button
+			<Fragment>
+				<div className="c-imageselector">
+					<MediaUploadCheck>
+						<MediaUpload
+							onSelect={image => {
+								getLazySrcs(image.id, image_format).then(image => setAttributes({[attribute_key]: image}));
+							}}
+							allowedTypes={allowed_types}
+							value={[attribute_key].id}
+							render={({open}) => {
+								return (
+									<figure className={`c-imageselector__figure ${!image.id ? 'c-imageselector__figure--noimage' : ''}`}>
+										{
+											image.id &&
+											<img
+												className="c-imageselector__image"
 												onClick={open}
-												isDefault
-												isLarge
-												isPrimary
-											>
-												{_x(
-													'Bild auswählen',
-													'Admin component button text',
-													'sht'
-												)}
-											</Button>
-										)}
-										{image.id && (
-											<Fragment>
+												src={image.org[0]}
+												alt={image.alt}
+												/>
+										}
+										<div className="c-imageselector__buttons">
+											{
+												!image.id &&
 												<Button
 													onClick={open}
 													isDefault
 													isLarge
-												>
-													{_x(
-														'Bild ersetzen',
-														'Admin component button text',
-														'sht'
-													)}
+													isPrimary>
+														{_x('Bild auswählen', 'Admin component button text', 'sha')}
 												</Button>
-												<Button
-													onClick={() =>
-														setAttributes({
-															image: {
-																id: false
-															}
-														})
-													}
-													isDefault
-													isSmall
-												>
-													{_x(
-														'Bild entfernen',
-														'Admin component button text',
-														'sht'
-													)}
-												</Button>
-											</Fragment>
-										)}
-									</div>
-								</figure>
-							);
-						}}
-					/>
-				</MediaUploadCheck>
-			</div>
+											}
+											{
+												image.id &&
+												<Fragment>
+													<Button
+														onClick={open}
+														isDefault
+														isLarge>
+														{_x('Bild ersetzen', 'Admin component button text', 'sha')}
+													</Button>
+													<Button
+														onClick={() => setAttributes({image: {id: false}})}
+														isDefault
+														isSmall>
+														{_x('Bild entfernen', 'Admin component button text', 'sha')}
+													</Button>
+												</Fragment>
+											}
+										</div>
+									</figure>
+								);
+							}}
+						/>
+					</MediaUploadCheck>
+				</div>
+			</Fragment>
 		);
 	}
 }
