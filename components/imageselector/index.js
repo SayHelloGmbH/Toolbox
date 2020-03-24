627801447e1aa65b01ead0edd4d38b7322d63ef5
Mark Howells-Mead <mark@sayhello.ch>
2020-03-24 15:17:01 +0100

    Update index.js

--- components/imageselector/index.js
+++ components/imageselector/index.js
@@ -1,11 +1,19 @@
 /**
- * Media selector for Say Hello components
- * mark@sayhello.ch 26.8.2019
+ * Image selector for Say Hello components
+ * mark@sayhello.ch 24.3.2020
+ *
+ * This version uses the core data store and should be used if the
+ * image is being rendered via ServerSideRender.
+ *
+ * The matching attribute (e.g. 'image') must be an Number, not an Object.
+ *
+ * Use our LazyImageImageSelector component if you need to use the
+ * component with rednering in the editor. In that case,
+ * the matching attribute must be an Object.
  *
  * Usage:
 <ImageSelector
 	attributes={attributes}
-	image={attributes.image}
 	setAttributes={setAttributes}
 	attributeKey="image"
 	allowedTypes={['image/jpg', 'image/jpeg']}
@@ -14,7 +22,6 @@
  * OR
 <ImageSelector
 	attributes={attributes}
-	image={attributes.image}
 	setAttributes={setAttributes}
 />
  */
@@ -23,10 +30,25 @@ import { Button } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import { MediaUploadCheck, MediaUpload } from '@wordpress/block-editor';
 import { _x } from '@wordpress/i18n';
+import { select } from '@wordpress/data';
 
-import { getLazySrcs } from './LazyImage.jsx';
+// Get the image data using the core data store
+function PreviewImage( { image } ) {
+	const imageData = select( 'core' ).getMedia( image );
+	return (
+		<Fragment>
+			{
+				!!imageData &&
+				<img
+					src={imageData.media_details.sizes.medium.source_url}
+					alt={imageData.alt_text}
+					/>
+			}
+		</Fragment>
+	);
+}
 
-export class ImageSelector extends Component {
+export default class ImageSelector extends Component {
 
 	constructor( props ) {
 		super( ...arguments );
@@ -39,12 +61,11 @@ export class ImageSelector extends Component {
 			allowedTypes,
 			attributes,
 			attributeKey,
-			image,
 			imageFormat,
 			setAttributes
 		} = this.props;
 
-		const allowed_types = allowedTypes || [ 'image/jpg', 'image/jpeg' ];
+		const allowed_types = allowedTypes || [ 'image' ];
 		const attribute_key = attributeKey || 'image';
 		const image_format = imageFormat || 'full';
 
@@ -53,26 +74,24 @@ export class ImageSelector extends Component {
 				<div className="c-imageselector">
 					<MediaUploadCheck>
 						<MediaUpload
-							onSelect={image => {
-								getLazySrcs(image.id, image_format).then(image => setAttributes({[attribute_key]: image}));
+							onSelect={media => {
+								setAttributes({ [attribute_key]: media.id });
 							}}
 							allowedTypes={allowed_types}
-							value={[attribute_key].id}
+							value={attributes[attribute_key].id}
 							render={({open}) => {
+
+								const image = attributes[attribute_key];
+
 								return (
-									<figure className={`c-imageselector__figure ${!image.id ? 'c-imageselector__figure--noimage' : ''}`}>
+									<figure className={`c-imageselector__figure ${!image ? 'c-imageselector__figure--noimage' : ''}`}>
 										{
-											image.id &&
-											<img
-												className="c-imageselector__image"
-												onClick={open}
-												src={image.org[0]}
-												alt={image.alt}
-												/>
+											!!image &&
+											<PreviewImage image={ image } />
 										}
 										<div className="c-imageselector__buttons">
 											{
-												!image.id &&
+												!image &&
 												<Button
 													onClick={open}
 													isDefault
@@ -82,7 +101,7 @@ export class ImageSelector extends Component {
 												</Button>
 											}
 											{
-												image.id &&
+												!!image &&
 												<Fragment>
 													<Button
 														onClick={open}
@@ -91,9 +110,9 @@ export class ImageSelector extends Component {
 														{_x('Bild ersetzen', 'Admin component button text', 'sha')}
 													</Button>
 													<Button
-														onClick={() => setAttributes({image: {id: false}})}
-														isDefault
-														isSmall>
+														onClick={() => setAttributes({image: 0})}
+														isLink
+														isDestructive>
 														{_x('Bild entfernen', 'Admin component button text', 'sha')}
 													</Button>
 												</Fragment>
