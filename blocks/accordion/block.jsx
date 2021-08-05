548595500b13a088f04ec17573df64c8cbdfc226
Mark Howells-Mead <mark@sayhello.ch>
2021-08-05 18:40:30 +0200

    Update block.jsx

--- blocks/accordion/block.jsx
+++ blocks/accordion/block.jsx
@@ -5,95 +5,113 @@ import { _x } from '@wordpress/i18n';
 import { calendar as icon } from '@wordpress/icons';
 import classnames from 'classnames';
 
-const blockName = 'sht/programme-accordion';
+const blockName = 'sht/accordion';
+const classNameBase = getBlockDefaultClassName(blockName);
 
-registerBlockType( blockName, {
-	title: _x( 'Akkordeon', 'Block title', 'sha' ),
-	description: _x( 'Eine Gruppe an Unterelemente, die der Seitenbesucher auf- und zuklappen kann.', 'Block title', 'sha' ),
-	icon,
-	category: 'common',
-	supports: {
-		align: false,
-		html: false,
-	},
-	attributes: {
-		blockID: {
-			type: 'string'
-		},
-		title: {
-			type: 'string',
-			default: _x('Akkordeon-Titel', 'Block title attribute default value', 'sha')
-		}
-	},
-	edit: class extends Component {
+registerBlockType(blockName, {
+    title: _x('Akkordeon', 'Block title', 'sha'),
+    description: _x(
+        'Eine Gruppe an Unterelemente, die der Seitenbesucher auf- und zuklappen kann.',
+        'Block title',
+        'sha'
+    ),
+    icon,
+    category: 'common',
+    supports: {
+        align: false,
+        html: false,
+    },
+    attributes: {
+        blockID: {
+            type: 'string',
+        },
+        title: {
+            type: 'string',
+            default: _x('Akkordeon-Titel', 'Block title attribute default value', 'sha'),
+        },
+    },
+    edit: class extends Component {
+        constructor(props) {
+            super(...arguments);
+            this.props = props;
+        }
 
-		constructor( props ) {
-			super( ...arguments );
-			this.props = props;
-		}
+        render() {
+            const { attributes, className, setAttributes } = this.props;
 
-		render() {
+            const { title } = attributes;
 
-			const classNameBase = getBlockDefaultClassName( blockName );
+            setAttributes({ blockID: this.props.clientId });
 
-			const { attributes, className, setAttributes } = this.props;
+            return (
+                <div className={className}>
+                    <RichText
+                        tagName='h2'
+                        placeholder={_x(
+                            'Geben Sie einen Titel für diesen Akkordeon-Block ein',
+                            'Field placeholder',
+                            'sha'
+                        )}
+                        className={`${classNameBase}__title`}
+                        value={title}
+                        allowedFormats={[]}
+                        multiline={false}
+                        keepPlaceholderOnFocus={true}
+                        onChange={value => {
+                            setAttributes({ title: value });
+                        }}
+                    />
+                    <InnerBlocks allowedBlocks={['core/paragraph', 'core/heading', 'core/image']} />
+                </div>
+            );
+        }
+    },
+    save({ attributes, className }) {
+        const wrapperClassName = classnames({
+            [className]: true,
+            [`c-accordion`]: true,
+        });
 
-			const { title } = attributes;
+        const { blockID, title } = attributes;
 
-			setAttributes({ blockID: this.props.clientId });
+        const the_title =
+            title ??
+            _x(
+                '[ Akkordeon-Titel nicht gesetzt ]',
+                'Block title attribute default value (save)',
+                'sha'
+            );
 
-			return (
-				<div className={className}>
-					<RichText
-						tagName='h2'
-						placeholder={_x('Geben Sie einen Titel für diesen Akkordeon-Block ein', 'Field placeholder', 'sha')}
-						className={`${classNameBase}__title`}
-						value={title}
-						allowedFormats={[]}
-						multiline={ false }
-						keepPlaceholderOnFocus={true}
-						onChange={(value) => {
-							setAttributes({title: value});
-						}}
-					/>
-					<InnerBlocks
-						allowedBlocks={['core/paragraph','core/heading','core/image']}
-						/>
-				</div>
-			);
-		}
-	},
-	save( { attributes, className } ) {
+        return (
+            <div className={wrapperClassName} data-accordion>
+                <div className={`${classNameBase}__header c-accordion__header`}>
+                    <RichText.Content
+                        className={`${classNameBase}__title c-accordion__header`}
+                        value={the_title}
+                        tagName='h2'
+                    />
+                    <button
+                        data-accordion-toggler
+                        className={`${classNameBase}__trigger c-accordion__trigger`}
+                        aria-controls={`accordion-${blockID}`}
+                    >
+                        <span className='screen-reader-text'>
+                            {_x('Diesen Bereich zu-/aufklappen', 'Accordion button text', 'sht')}
+                        </span>
+                        <span
+                            className={`${classNameBase}__triggericon c-accordion__triggericon`}
+                        ></span>
+                    </button>
+                </div>
 
-		const classNameBase = getBlockDefaultClassName( blockName );
-
-		const wrapperClassName = classnames( {
-			[ className ]: true,
-			[ `c-accordion` ]: true
-		} );
-
-		const { blockID, title } = attributes;
-
-		const the_title = title ?? _x('[ Akkordeon-Titel nicht gesetzt ]', 'Block title attribute default value (save)', 'sha');
-
-		return (
-			<div className={wrapperClassName} data-accordion>
-				<div className={`${classNameBase}__header c-accordion__header`}>
-					<RichText.Content
-						className={`${classNameBase}__title c-accordion__header`}
-						value={the_title}
-						tagName='h2'
-					/>
-					<button data-accordion-toggler className={`${classNameBase}__trigger c-accordion__trigger`} aria-controls={`accordion-${blockID}`}>
-						<span className="screen-reader-text">{_x('Diesen Bereich zu-/aufklappen', 'Accordion button text', 'sht')}</span>
-						<span className={`${classNameBase}__triggericon c-accordion__triggericon`}></span>
-					</button>
-				</div>
-
-				<div data-accordion-content className={`${classNameBase}__inner c-accordion__inner`} id={`accordion-${blockID}`}>
-					<InnerBlocks.Content />
-				</div>
-			</div>
-		);
-	}
-} );
+                <div
+                    data-accordion-content
+                    className={`${classNameBase}__inner c-accordion__inner`}
+                    id={`accordion-${blockID}`}
+                >
+                    <InnerBlocks.Content />
+                </div>
+            </div>
+        );
+    },
+});
