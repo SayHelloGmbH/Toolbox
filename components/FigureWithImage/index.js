6bf60d7ffcc4bf86caec821b99a0ba2511e40948
Mark Howells-Mead <mark@sayhello.ch>
2022-02-01 16:07:17 +0100

    Create index.js

---
+++ components/FigureWithImage/index.js
@@ -0,0 +1,46 @@
+/**
+ * Generates an HTML FIGURE and IMAGE set based on
+ * a valid image object returned from the WordPress REST API
+ *
+ * Pass in the classNameBase (which is used to construct the
+ * CSS class names for the elements) and the image object.
+ *
+ * Usage:
+
+{!!image.id && (
+    <FigureWithImage classNameBase={classNameBase} image={image} />
+)}
+
+ * mark@sayhello.ch 1.2.2022
+ */
+
+export const FigureWithImage = ({ classNameBase, image }) => {
+    if (!image.id) {
+        return '';
+    }
+
+    const { org, srcset, attributes } = image;
+    const { width, height, alt } = attributes;
+
+    // Join all of the available sizes together to form a srcset string
+    const srcSetAttribute = srcset => {
+        return Object.entries(srcset)
+            .map(([key, entry]) => `${entry} ${key}w`)
+            .join(', ');
+    };
+
+    return (
+        <figure className={`${classNameBase}__figure`}>
+            <img
+                src={org[0]}
+                srcset={srcSetAttribute(srcset)}
+                alt={alt}
+                className={`${classNameBase}__image`}
+                width={width}
+                height={height}
+            />
+        </figure>
+    );
+};
+
+export default FigureWithImage;
