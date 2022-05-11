35a432c8507f8f2db5ac9a06e3a5ef740c8723e9
Mark Howells-Mead <mark@sayhello.ch>
2022-05-11 15:52:41 +0200

    Update index.js

--- Gutenberg/components/imageselector/index.js
+++ Gutenberg/components/imageselector/index.js
@@ -1,15 +1,11 @@
 /**
  * Image selector for Say Hello components
- * mark@sayhello.ch 24.3.2020
+ * mark@sayhello.ch 11.5.2022
  *
  * This version uses the core data store and should be used if the
  * image is being rendered via ServerSideRender.
  *
- * The matching attribute (e.g. 'image') must be an Number, not an Object.
- *
- * Use our LazyImageImageSelector component if you need to use the
- * component with rednering in the editor. In that case,
- * the matching attribute must be an Object.
+ * The matching attribute (e.g. 'image') must be a Number, not an Object.
  *
  * Usage:
 <ImageSelector
@@ -17,7 +13,6 @@
 	setAttributes={setAttributes}
 	attributeKey="image"
 	allowedTypes={['image/jpg', 'image/jpeg']}
-	imageFormat="full"
 />
  * OR
 <ImageSelector
@@ -26,105 +21,115 @@
 />
  */
 
-import { Button } from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
+import { BaseControl, Button } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
 import { MediaUploadCheck, MediaUpload } from '@wordpress/block-editor';
 import { _x } from '@wordpress/i18n';
-import { select } from '@wordpress/data';
+import { withSelect } from '@wordpress/data';
 
-// Get the image data using the core data store
-function PreviewImage( { image } ) {
-	const imageData = select( 'core' ).getMedia( image );
-	return (
-		<Fragment>
-			{
-				!!imageData &&
-				<img
-					src={imageData.media_details.sizes.medium.source_url}
-					alt={imageData.alt_text}
-					/>
-			}
-		</Fragment>
-	);
-}
+const classNameBase = 'c-imageselector';
 
-export default class ImageSelector extends Component {
+import './index.scss';
 
-	constructor( props ) {
-		super( ...arguments );
-		this.props = props;
-	}
+const PreviewImage = props => {
+    const { image: imageData } = props;
 
-	render() {
+    if (
+        !imageData ||
+        !imageData.media_details ||
+        !imageData.media_details.sizes ||
+        !imageData.media_details.sizes.medium ||
+        !imageData.media_details.sizes.medium.source_url
+    ) {
+        return '';
+    }
 
-		const {
-			allowedTypes,
-			attributes,
-			attributeKey,
-			imageFormat,
-			setAttributes
-		} = this.props;
+    const { source_url } = imageData.media_details.sizes.medium;
+    const { alt_text } = imageData;
 
-		const allowed_types = allowedTypes || [ 'image' ];
-		const attribute_key = attributeKey || 'image';
-		const image_format = imageFormat || 'full';
+    return <img src={source_url} alt={alt_text} />;
+};
 
-		return (
-			<Fragment>
-				<div className="c-imageselector">
-					<MediaUploadCheck>
-						<MediaUpload
-							onSelect={media => {
-								setAttributes({ [attribute_key]: media.id });
-							}}
-							allowedTypes={allowed_types}
-							value={attributes[attribute_key].id}
-							render={({open}) => {
+const ImageSelector = props => {
+    const { allowedTypes, attributes, attributeKey, setAttributes, label, preview_image } = props;
 
-								const image = attributes[attribute_key];
+    const allowed_types = allowedTypes || ['image'];
+    const attribute_key = attributeKey || 'image';
 
-								return (
-									<figure className={`c-imageselector__figure ${!image ? 'c-imageselector__figure--noimage' : ''}`}>
-										{
-											!!image &&
-											<PreviewImage image={ image } />
-										}
-										<div className="c-imageselector__buttons">
-											{
-												!image &&
-												<Button
-													onClick={open}
-													isDefault
-													isLarge
-													isPrimary>
-														{_x('Bild auswählen', 'Admin component button text', 'sha')}
-												</Button>
-											}
-											{
-												!!image &&
-												<Fragment>
-													<Button
-														onClick={open}
-														isDefault
-														isLarge>
-														{_x('Bild ersetzen', 'Admin component button text', 'sha')}
-													</Button>
-													<Button
-														onClick={() => setAttributes({image: 0})}
-														isLink
-														isDestructive>
-														{_x('Bild entfernen', 'Admin component button text', 'sha')}
-													</Button>
-												</Fragment>
-											}
-										</div>
-									</figure>
-								);
-							}}
-						/>
-					</MediaUploadCheck>
-				</div>
-			</Fragment>
-		);
-	}
-}
+    return (
+        <BaseControl label={label}>
+            <div className={classNameBase}>
+                <MediaUploadCheck>
+                    <MediaUpload
+                        onSelect={media => {
+                            setAttributes({ [attribute_key]: media.id });
+                        }}
+                        allowedTypes={allowed_types}
+                        value={attributes[attribute_key].id}
+                        render={({ open }) => {
+                            const image = attributes[attribute_key];
+
+                            return (
+                                <figure
+                                    className={`${classNameBase}__figure ${
+                                        !image ? `${classNameBase}__figure--noimage` : ''
+                                    }`}
+                                >
+                                    {!!preview_image && <PreviewImage image={preview_image} />}
+                                    <div className={`${classNameBase}__buttons`}>
+                                        {!image && (
+                                            <Button onClick={open} variant='primary'>
+                                                {_x(
+                                                    'Bild auswählen',
+                                                    'Admin component button text',
+                                                    'sha'
+                                                )}
+                                            </Button>
+                                        )}
+                                        {!!image && (
+                                            <Fragment>
+                                                <Button onClick={open} variant='primary'>
+                                                    {_x(
+                                                        'Bild ersetzen',
+                                                        'Admin component button text',
+                                                        'sha'
+                                                    )}
+                                                </Button>
+                                                <Button
+                                                    onClick={() => setAttributes({ image: 0 })}
+                                                    variant='link'
+                                                    isDestructive
+                                                >
+                                                    {_x(
+                                                        'Bild entfernen',
+                                                        'Admin component button text',
+                                                        'sha'
+                                                    )}
+                                                </Button>
+                                            </Fragment>
+                                        )}
+                                    </div>
+                                </figure>
+                            );
+                        }}
+                    />
+                </MediaUploadCheck>
+            </div>
+        </BaseControl>
+    );
+};
+
+export default withSelect((select, props) => {
+    const { attributes, attributeKey } = props;
+
+    const image = attributes[attributeKey];
+
+    if (!!image) {
+        return {
+            ...props,
+            preview_image: select('core').getMedia(image),
+        };
+    }
+
+    return props;
+})(ImageSelector);
